d8dd8b1de0e9dc9ccbc2f83769bddef50ac3f520
jan <jan@d6a85330-1cd7-4390-b269-eb986cef6744>
2008-05-24 14:31:49 +0000

    remote working over thin lines sucks
    git-svn-id: file:///home/jan/tmp/HTW/trunk@31 d6a85330-1cd7-4390-b269-eb986cef6744

--- b3/smtprelay.c
+++ b3/smtprelay.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <signal.h>
+#include <netdb.h>
 #include "smtprelay.h"
 #include "connection.h"
 
@@ -13,10 +14,92 @@ void put_err(char *src){
   }
   perror("");
 }
+void put_err_str(char *err){
+  if(err != NULL){
+    fprintf(stderr, ERROR_PREF "%s \n", err);
+  } else {
+    fprintf(stderr, ERROR_PREF " Unspecified\n");
+  }
+}
+
+char *port_opt(char *buff, char *err){
+  char *port = malloc(sizeof(char)*6);
+  int is_digit = 1, i;
+  buff[5] = '\0';
+
+  for(i = 0; i < 5; i++){
+    if(!isdigit(buff[i])){
+      is_digit = 0;
+      break;
+    }
+  }
+
+  if(is_digit || atoi(buff) > 65535 || atoi(buff) < 1){
+    put_err_str(err);
+    free(port);
+    return NULL;
+  }
+  strcpy(port, buff);
+  return port;
+}
+
+char *host_opt(char *buff, char *err){
+  char *addr = malloc(sizeof(char)*16);
+  int is_ip = 0, i;
+  struct hostent *hent;
+
+  if (strlen(buff) == 15){
+    is_ip = 1;
+    for(i = 0; i < 4 ; i++){
+      offset = i * 4;
+      for(j = 0; j < 3; j++){
+	if(!isdigit(buff[(offset+j)])){
+	  is_ip = 0;
+	  break;
+	}
+      }
+      if(!is_ip || !strncmp(&(buff[offset + 4]))){
+	is_ip = 0;
+	break;
+      }
+    }
+  }
+
+  if (is_ip){
+    strcmp(addr, buff);
+    return addr;
+  }
+  
+  if((hent = gethostbyname(buff)) == NULL){
+    put_err_str(err);
+    free(addr);
+    return NULL;
+  }
+  
+  tmp = ntohl(((struct in_addr*)hent->h_addr_list[0])->s_addr);
+
+  for (i = 0; i < 4; i++){
+    offset = i*4
+    snprintf(buff+offset, 4, "%d", (tmp >> (8*i)) & 0xFF);
+    if(i){
+      *(buff+offset-1) = '.';
+    }
+
+  printf("%d\n", bla);
+
+  }
 
 
 
-int process_opt(int argc, char *argv[], char **addr, char **port){
+
+}
+
+int process_opt(int argc, char *argv[], char **addr_ptr, char **port_ptr){
+  char *addr = malloc(sizeof(char)*16);
+  char buff[1024];
+
+  
+
   if (argc == 3) {
     *port = argv[2];
     *addr = argv[1];
